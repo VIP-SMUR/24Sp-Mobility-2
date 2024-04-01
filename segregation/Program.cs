d3ee64a4e38e199a9569d3715cfef1f77aeee692
handasahil <113319786+handasahil@users.noreply.github.com>
2024-04-01 11:55:04 -0400

    initial seg files

---
+++ segregation/Program.cs
@@ -0,0 +1,18 @@
+﻿using Itinero; 
+using Itinero.IO.Osm;
+using Itinero.Osm.Vehicles;
+
+var routerDb = new RouterDb();
+var profile = Vehicle.Car.Fastest(); // the default OSM car profile.
+
+  using (var stream = new FileInfo("quebec-latest.osm.pbf").OpenRead())
+  {
+      // create the network for cars.
+      routerDb.LoadOsmData(stream, Vehicle.Car);
+  }
+
+  // write the routerdb to disk.
+  using (var stream = new FileInfo(@"quebec.routerdb").Open(FileMode.Create))
+  {
+      routerDb.Serialize(stream);
+  }
